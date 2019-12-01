79e059d1a6774f09db566b745215c02bc71375cd
Jay Asbury <vbjay.net@gmail.com>
2019-12-01 13:55:59 -0500

    Show that connection works

--- DB Setup/Form1.vb
+++ DB Setup/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using cn As New SqlConnection(ConfigurationManager.ConnectionStrings("MyDb").ConnectionString)
+            cn.Open()
+
+            MessageBox.Show(cn.ConnectionString)
+
+        End Using
+    End Sub
 End Class
